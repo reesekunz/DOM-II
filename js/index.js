ffee848a6d306f1dbbbc2307d951ab72f79dd7cb
Reese Kunz <reesekunz21@gmail.com>
2019-07-09 14:26:35 -0700

    halfway done refactoring event listeners

--- js/index.js
+++ js/index.js
@@ -1,67 +1,106 @@
-// Your code goes here
-
-// Refactor 
 // #1 Nav links change to red on mouseover
 
 let navLinks = document.querySelectorAll(".nav-link");
 function changeLink(link, color) {
- link.style.color = `${color}`;
+  link.style.color = `${color}`;
 }
 
 navLinks.forEach(link => {
- link.addEventListener("mouseover", () => {
-   changeLink(link, "darkred");
- });
+  link.addEventListener("mouseover", () => {
+    changeLink(link, "darkred");
+  });
 
-   link.addEventListener("mouseleave", () => {
-   changeLink(link, "black");
- })
-  
+  link.addEventListener("mouseleave", () => {
+    changeLink(link, "black");
+  });
 });
 
-
-// #2 Each header increases in size on mouseover 
+// #2 Each header increases in size on mouseover
 
 let allHeaders = document.querySelectorAll(".headerclass");
 
 function changeSize(header, fontSize) {
- header.style.fontSize = `${fontSize}`;
+  header.style.fontSize = `${fontSize}`;
 }
 
-allHeaders.forEach(header=> {
- header.addEventListener("mouseover", () => {
-   changeSize(header, "4rem");
- });
+allHeaders.forEach(header => {
+  header.addEventListener("mouseover", () => {
+    changeSize(header, "4rem");
+  });
 
-   header.addEventListener("mouseleave", () => {
-   changeSize(header, "3rem");
- })
-  
+  header.addEventListener("mouseleave", () => {
+    changeSize(header, "3rem");
+  });
 });
 
 // #3 Each header font turns cursive on click 
 
 function changeFont(header, fontFamily) {
-    header.style.fontFamily = `${fontFamily}`;
-   }
-   
-   allHeaders.forEach(header=> {
-    header.addEventListener("click", () => {
-      changeFont(header, "cursive");
+  header.style.fontFamily = `${fontFamily}`;
+}
+
+allHeaders.forEach(header => {
+  header.addEventListener("click", () => {
+    changeFont(header, "cursive");
+  });
+});
+
+allHeaders.forEach(header => {
+    header.addEventListener("dblclick", () => {
+      changeFont(header, "Times New Roman");
     });
-   
-     
-   });
+  });
+
+// #4 Fun Bus logo text changes on copy
+
+let funBusLogo = document.querySelector(".logo-heading");
+
+function changeText ()
+{funBusLogo.textContent = "Don't plagiarize!!"}
+
+funBusLogo.addEventListener("copy", changeText);
 
 
-      
-
-// End of Refactoring 
+// #5 Top header text changes on mouseover 
 
 
-footerText.addEventListener = ("focus", footerTextChange);
 
-// Nesting nav link with event propagation 
+let welcomeHeader = document.querySelector("h2");
+
+
+function changeHeaderText(){
+    welcomeHeader.textContent = "More fun than an Uber, cheaper than a limo";
+  }
+
+  function changeHeaderTextBack(){
+    welcomeHeader.textContent = "Welcome to fun Bus!";
+  }
+
+
+  welcomeHeader.addEventListener("mouseover", changeHeaderText);
+
+  welcomeHeader.addEventListener("mouseleave", changeHeaderTextBack);
+  
+  
+
+
+
+
+
+// End of Refactoring
+
+
+
+
+
+
+
+
+
+
+
+
+// Nesting nav link with event propagation
 
 let navLink1 = document.querySelector(".nav-link");
 let navBar1 = document.querySelector(".nav");
@@ -69,20 +108,18 @@ let navBar1 = document.querySelector(".nav");
 console.log(navLink1);
 console.log(navBar1);
 
+navLink1.addEventListener("click", e => {
+  console.log("nav link 1 clicked");
+  e.stopPropagation();
+});
 
-navLink1.addEventListener("click", (e) =>
-{console.log("nav link 1 clicked")
-e.stopPropagation();
-})
+// Navigation prevent default
 
-// Navigation prevent default 
-
-let navBar = document.querySelector('header nav');
+let navBar = document.querySelector("header nav");
 
 console.log(navBar);
 
-navBar.addEventListener('click', event =>
-{event.preventDefault()
-console.log('Clicked!')
-} ); 
-
+navBar.addEventListener("click", event => {
+  event.preventDefault();
+  console.log("Clicked!");
+});
