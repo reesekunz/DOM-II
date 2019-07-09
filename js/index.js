186b79b047e4434a2a632f373483d6f1c9cb45e6
Reese Kunz <reesekunz21@gmail.com>
2019-07-09 14:03:04 -0700

    added event listeners to each header

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-
+// Refactor 
 // #1 Nav links change to red on mouseover
 
 let navLinks = document.querySelectorAll(".nav-link");
@@ -20,113 +20,44 @@ navLinks.forEach(link => {
 });
 
 
-// #1 - Header changes to yellow on click 
+// #2 Each header increases in size on mouseover 
 
-let funBus = document.querySelector("h2");
+let allHeaders = document.querySelectorAll(".headerclass");
 
-function changeColor()
-{funBus.style.color = "yellow";}
+function changeSize(header, fontSize) {
+ header.style.fontSize = `${fontSize}`;
+}
 
-funBus.addEventListener("click", changeColor);
+allHeaders.forEach(header=> {
+ header.addEventListener("mouseover", () => {
+   changeSize(header, "4rem");
+ });
 
+   header.addEventListener("mouseleave", () => {
+   changeSize(header, "3rem");
+ })
+  
+});
 
-// #2 Header text changes on mouseover
+// #3 Each header font turns cursive on click 
 
-function changeText()
-{funBus.textContent = "MORE FUN THAN AN UBER, CHEAPER THAN A LIMO";}
-
-funBus.addEventListener("mouseover", changeText);
-
-
-// #3 Magic School bus image appears on double click 
-
-let busImage = document.querySelector(".intro img");
-
-console.log(busImage);
-
-function changeImage()
-{busImage.src = "http://www.govst.edu/uploadedImages/About/Center_for_Performing_Arts/Arts_and_Education/School_Performances/Magic_School_Bus_767x340.jpg"};
-
-busImage.addEventListener("dblclick", changeImage);
-
-
-// #4 Header paragraph text changes on keydown
-
-let headerText = document.querySelector(".intro p");
-
-console.log(headerText);
-
-function editParagraph()
-{headerText.textContent = "This bus was the most fun out of all the busses. And Ive been on a lot of busses - Kevin Malone"};
-
-headerText.addEventListener("keydown", editParagraph);
-
-// #5 Change header text on wheel 
-
-let headerText2 = document.querySelector(".text-content h2");
-
-console.log(headerText2);
-
-function lebronJames ()
-{headerText2.textContent = "LeBron James took this bad boy to every home game when he was in Miami."};
-
-headerText2.addEventListener("wheel", lebronJames);
+function changeFont(header, fontFamily) {
+    header.style.fontFamily = `${fontFamily}`;
+   }
+   
+   allHeaders.forEach(header=> {
+    header.addEventListener("click", () => {
+      changeFont(header, "cursive");
+    });
+   
+     
+   });
 
 
-// #6 Change second image to LeBron James on a bus on scroll 
+      
 
-let image2 = document.querySelector(".img-content img");
+// End of Refactoring 
 
-console.log(image2);
-
-function lebronJamesBus ()
-{image2.src = "https://cavsnation.com/wp-content/uploads/2014/08/lebron-paul-bus.jpg"};
-
-window.addEventListener("scroll", lebronJamesBus);
-
-
-// #7 Change Fun Bus text on load 
-
-let funBusNav = document.querySelector(".logo-heading")
-
-console.log(funBusNav);
-
-function gusBus ()
-{funBusNav.textContent = "Gus Bus!"};
-
-funBusNav.addEventListener("load", gusBus);
-
-
-// #8 Change paragraph color on select start 
-
-let paragraphColor = document.querySelector(".text-content p");
-
-console.log(paragraphColor);
-
-function paragraphColorChange()
-{paragraphColor.style.color = "red";}
-
-paragraphColor.addEventListener("selectstart", paragraphColorChange);
-
-// #9 Change font size on resize for every paragraph 
-
-let paragraphFontSize = document.querySelectorAll("p");
-
-console.log(paragraphFontSize);
-
-function paragraphFontSizeChange ()
-{paragraphFontSize.style.fontSize = "2.2rem";}
-
-window.addEventListener = ("resize", paragraphFontSizeChange);
-
-// #10 Change footer text on focus 
-
-let footerText = document.querySelector(".footer p");
-
-console.log(footerText);
-
-function footerTextChange ()
-{footerText.textContent = "You better get on that fun bus boi";}
 
 footerText.addEventListener = ("focus", footerTextChange);
 
