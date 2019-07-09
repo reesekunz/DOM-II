022772e421eea5a429f8d999efebc41b0bc9c3b5
Reese Kunz <reesekunz21@gmail.com>
2019-07-09 14:56:56 -0700

    fixed formatting, DOM II finished

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 // #1 Nav links change to red on mouseover
 
 let navLinks = document.querySelectorAll(".nav-link");
@@ -34,7 +33,7 @@ allHeaders.forEach(header => {
   });
 });
 
-// #3 Each header font turns cursive on click 
+// #3 Each header font turns cursive on click
 
 function changeFont(header, fontFamily) {
   header.style.fontFamily = `${fontFamily}`;
@@ -47,80 +46,76 @@ allHeaders.forEach(header => {
 });
 
 allHeaders.forEach(header => {
-    header.addEventListener("dblclick", () => {
-      changeFont(header, "Times New Roman");
-    });
+  header.addEventListener("dblclick", () => {
+    changeFont(header, "Times New Roman");
   });
+});
 
 // #4 Fun Bus logo text changes on copy
 
 let funBusLogo = document.querySelector(".logo-heading");
 
-function changeText ()
-{funBusLogo.textContent = "Don't plagiarize!!"}
+function changeText() {
+  funBusLogo.textContent = "Don't plagiarize!!";
+}
 
 funBusLogo.addEventListener("copy", changeText);
 
-
-// #5 Top header text changes on mouseover 
-
-
+// #5 Top header text changes on mouseover
 
 let welcomeHeader = document.querySelector("h2");
 
+function changeHeaderText() {
+  welcomeHeader.textContent = "More fun than an Uber, cheaper than a limo";
+}
 
-function changeHeaderText(){
-    welcomeHeader.textContent = "More fun than an Uber, cheaper than a limo";
-  }
+function changeHeaderTextBack() {
+  welcomeHeader.textContent = "Welcome to fun Bus!";
+}
 
-  function changeHeaderTextBack(){
-    welcomeHeader.textContent = "Welcome to fun Bus!";
-  }
+welcomeHeader.addEventListener("mouseover", changeHeaderText);
 
+welcomeHeader.addEventListener("mouseleave", changeHeaderTextBack);
 
-  welcomeHeader.addEventListener("mouseover", changeHeaderText);
+// #6 Magic School bus image appears on double click
 
-  welcomeHeader.addEventListener("mouseleave", changeHeaderTextBack);
-
-  // #6 Magic School bus image appears on double click
-  
 let busImage = document.querySelector(".intro img");
 
-function changeImage()
-{busImage.src = "http://www.govst.edu/uploadedImages/About/Center_for_Performing_Arts/Arts_and_Education/School_Performances/Magic_School_Bus_767x340.jpg"};
+function changeImage() {
+  busImage.src =
+    "http://www.govst.edu/uploadedImages/About/Center_for_Performing_Arts/Arts_and_Education/School_Performances/Magic_School_Bus_767x340.jpg";
+}
 
 busImage.addEventListener("dblclick", changeImage);
 
-// #7 Each paragraph text changes on paste 
+// #7 Each paragraph text changes on paste
 
 let paragraphs = document.querySelectorAll("p");
 function changeP(paragraph, text) {
- paragraph.textContent = `${text}`;
+  paragraph.textContent = `${text}`;
 }
 
 paragraphs.forEach(paragraph => {
- paragraph.addEventListener("paste", () => {
-   changeP(paragraph, "Dude get your own content");
- });
-
+  paragraph.addEventListener("paste", () => {
+    changeP(paragraph, "Dude get your own content");
+  });
 });
 
 // #8 Change button background colors on mouseover
 
 let buttons = document.querySelectorAll(".btn");
 function changeButton(button, color) {
- button.style.backgroundColor = `${color}`;
+  button.style.backgroundColor = `${color}`;
 }
 
 buttons.forEach(button => {
- button.addEventListener("mouseover", () => {
-   changeButton(button, "yellow");
- });
+  button.addEventListener("mouseover", () => {
+    changeButton(button, "yellow");
+  });
 
-   button.addEventListener("mouseleave", () => {
-   changeButton(button, "black");
- })
-  
+  button.addEventListener("mouseleave", () => {
+    changeButton(button, "black");
+  });
 });
 
 // #9 Change second image to LeBron James on a bus on scroll
@@ -129,29 +124,30 @@ let image2 = document.querySelector(".img-content img");
 
 console.log(image2);
 
-function lebronJamesBus ()
-{image2.src = "https://cavsnation.com/wp-content/uploads/2014/08/lebron-paul-bus.jpg"};
+function lebronJamesBus() {
+  image2.src =
+    "https://cavsnation.com/wp-content/uploads/2014/08/lebron-paul-bus.jpg";
+}
 
 window.addEventListener("scroll", lebronJamesBus);
 
-// #10 Change last paragraph on mouseover 
+// #10 Change last paragraph on mouseover
 
 let island = document.querySelector(".island");
 
+function changeIsland() {
+  island.textContent =
+    "Honestly, don't even pick this one it's a waste of money";
+}
 
-function changeIsland(){
-    island.textContent = "Honestly, don't even pick this one it's a waste of money";
-  }
+function changeIslandBack() {
+  island.textContent =
+    "Expedition excursion design excursion fun, clean simple organized WordPress Travel colorful webdesign. Traveler blogger website design expedition clean excursion traveling.";
+}
 
-  function changeIslandBack(){
-    island.textContent = "Expedition excursion design excursion fun, clean simple organized WordPress Travel colorful webdesign. Traveler blogger website design expedition clean excursion traveling.";
-  }
+island.addEventListener("mouseover", changeIsland);
 
-
-  island.addEventListener("mouseover", changeIsland);
-
-  island.addEventListener("mouseleave", changeIslandBack);
-
+island.addEventListener("mouseleave", changeIslandBack);
 
 // Nesting nav link with event propagation
 
